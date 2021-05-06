e8a9b7ec061cdaf430b38b9f6e07e12f9889e01f
Sergio Alvarez <salvare736@gmail.com>
2021-05-06 15:51:22 -0400

    added react app for the front end

--- index.js
+++ index.js
@@ -2,18 +2,26 @@ console.log('Hello world!');
 
 require('dotenv').config();
 
+const path = require('path');
+
 const express = require('express');
 
 const server = express();
 
 const PORT = process.env.PORT || 4000;
 
+server.use(express.static(path.join(__dirname, 'client/build')));
+
 server.use(express.json());
 
 server.get('/api/*', (req, res) => {
   res.json({
-    message: 'Sergio from Web 41 checking in!',
-  })
+    specialMessage: 'Sergio from Web 41 checking in!',
+  });
+});
+
+server.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 });
 
 server.listen(PORT, () => {
